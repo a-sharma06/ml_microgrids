3664dbdfc12ab33f9688224f41318ae75f4709be
Akshay Sharma <a_sharma06@yahoo.com>
2018-05-10 01:15:37 -0400

    test

--- app.py
+++ app.py
@@ -75,6 +75,14 @@ def about():
               toolbar_location=None)
 
     graph = from_networkx(H, nx.spring_layout, scale = 100, center=(0,0))
+    
+    G=nx.karate_club_graph()
+    
+    plot = figure(title="Networkx Integration Demonstration", x_range=(-1.1,1.1), y_range=(-1.1,1.1),
+          tools="", toolbar_location=None)
+    
+    graph = from_networkx(G, nx.spring_layout, scale=2, center=(0,0))
+    
     plot.renderers.append(graph)    
     script, div = components(plot)
     
