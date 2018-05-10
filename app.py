e4bb8d72dacc161873a6c1470ec8a092c33b80ad
Akshay Sharma <a_sharma06@yahoo.com>
2018-05-10 01:03:13 -0400

    test

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def about():
     plot = figure(title="Networkx Integration Demonstration",
               toolbar_location=None)
 
-    graph = from_networkx(H, nx.spring_layout, scale=2, center=(0,0))
+    graph = from_networkx(H)
     plot.renderers.append(graph)    
     script, div = components(plot)
     
