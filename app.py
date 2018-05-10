95defc5ea8d800bf9e3b180c37bd1254f5c2e624
Akshay Sharma <a_sharma06@yahoo.com>
2018-05-09 22:01:12 -0400

    test

--- app.py
+++ app.py
@@ -29,37 +29,42 @@ def index():
 @app.route('/about', methods=['GET', 'POST'])
 def about():
     #ticker = 'GOOG'
-    ticker = request.form['ticker']
-    features = request.form.getlist('features')
-    lookup = dict([('open','Open'),('close','Close'),('adj_close','Adj. Open'),('adj_open','Adj. Close')])
-    cols = [lookup[x] for x in features]
-    cols.append('Date')
-    url = 'https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'.csv?start_date=2017-01-01&end_date=2017-12-31&order=asc&api_key='+os.environ['QUANDL_KEY']
-    urlData = requests.get(url).content
-    rawData = pd.read_csv(io.StringIO(urlData.decode('utf-8')))
-    rawData.Date = pd.to_datetime(rawData.Date)
-    rawData = rawData.loc[:,cols]
-    cols.remove('Date')
-    # create a new plot with a title and axis labels
-    p = figure(tools="pan,wheel_zoom,box_zoom,reset", title='Quandl WIKI EOD Stock Price - 2017 ', x_axis_label='Time', y_axis_label='Price', x_axis_type="datetime")
-
-    #i=0
-    # add a line renderer with legend and line thickness                                                                                                                                       
-    #for x in cols:
-    #    p.line(y = rawData[x], x = rawData.Date, legend = (ticker + ' ' + x + ' - Value'), line_width=2, line_color=Blues8[i])
-    #    i = i+1
+    #ticker = request.form['ticker']
+    bnames = request.form.getlist('name')
+    area = request.form.getlist('area')
+    latitude = request.form.getlist('latitude') 
+    longitude = request.form.getlist('longitude')
+    data = [ {name : {'area': area[x], 'latitude': latitude[x], 'longitude': latitude[x]}} for x,name in enumerate(bnames)]
     
-    for i,x in enumerate(cols,0):
-        p.line(y = rawData[x], x = rawData.Date, legend = (ticker + ' ' + x + ' - Value'), line_width=2, line_color=Blues8[i])     
+    #lookup = dict([('open','Open'),('close','Close'),('adj_close','Adj. Open'),('adj_open','Adj. Close')])
+    #cols = [lookup[x] for x in features]
+    #cols.append('Date')
+    #url = 'https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'.csv?start_date=2017-01-01&end_date=2017-12-31&order=asc&api_key='+os.environ['QUANDL_KEY']
+    #urlData = requests.get(url).content
+    #rawData = pd.read_csv(io.StringIO(urlData.decode('utf-8')))
+    #rawData.Date = pd.to_datetime(rawData.Date)
+    #rawData = rawData.loc[:,cols]
+    #cols.remove('Date')
+    ## create a new plot with a title and axis labels
+    #p = figure(tools="pan,wheel_zoom,box_zoom,reset", title='Quandl WIKI EOD Stock Price - 2017 ', x_axis_label='Time', y_axis_label='Price', x_axis_type="datetime")
 
-    p.legend.location = "top_left"
-    #p.legend.click_policy="hide"
+    ##i=0
+    ## add a line renderer with legend and line thickness                                                                                                                                       
+    ##for x in cols:
+    ##    p.line(y = rawData[x], x = rawData.Date, legend = (ticker + ' ' + x + ' - Value'), line_width=2, line_color=Blues8[i])
+    ##    i = i+1
+    
+    #for i,x in enumerate(cols,0):
+    #    p.line(y = rawData[x], x = rawData.Date, legend = (ticker + ' ' + x + ' - Value'), line_width=2, line_color=Blues8[i])     
+
+    #p.legend.location = "top_left"
+    ##p.legend.click_policy="hide"
 
     
-    script, div = components(p)
+    #script, div = components(p)
     
     # show the results
-    return render_template('about.html',script=script, div=div)
+    return render_template('about.html', result = data)
 
 if __name__ == '__main__':
    app.run()
